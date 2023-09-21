b53d233a978059c56f460c3487e7bbe8cd11fd53
Adrian Pauli <adrian.pauli@qumea.com>
2023-09-21 17:22:02 +0200

    Initial import

---
+++ src/main/java/com/example/ExampleResource.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @ConfigProperty(name = "value.test")
+    public String testValue;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return testValue;
+    }
+}
